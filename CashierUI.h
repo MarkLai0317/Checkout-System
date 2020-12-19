fd9b949cb92a99cc3ef2b2503a65f8f42c5508df
Mark Lai <laimark900317@gmail.com>
2020-12-19 13:48:46 +0800

    modify input

--- CashierUI.h
+++ CashierUI.h
@@ -2,6 +2,10 @@
 #include <vector>
 #include "GoodInventory.h"
 
+#define INVALID -100 
+#define BACK -1
+#define QUIT -2
+#define DELETE -3 
 
 
 class CashierUI {
@@ -20,27 +24,28 @@ class CashierUI {
 private:
 
 	// 1.Need to see if we have the chosen category.
-	//   If we don't have, it's alse a invalid input. need to ask input one more time
+	//   If we don't have, print warning and return INVALID
 	// 2.Return the valid category input.
-	//   Input 'q' means quit, return -1.
-	//   Input 'd' maens delete one goods have just choose, return -2
+	//   Input 'q' means quit, return QUIT.
+	//   Input 'd' maens delete one goods have just choose, return DELETE
 	// 3.when implementation, using string to get the input will be easier to identify invalid input
 	//   so do other input
 	int inputCategory();
 
 	// 1.Need to see if we have the chosen ID
-	//   If we don't have, it's alse a invalid input. need to ask input one more time
+	//   If we don't have, it's a invalid input--> return INVALID.
 	// 2.return the valid id input
-	//   input 'b' means back to the last page, return -1
+	//   input 'b' means back to the last page, return BACK
 	// 3.same as above 3.
 	int inputId();
 
 
 	// 1.Need to see if we have the chosen Quantity.
-	//   If we don't have enough goods, it's also an invalid input. 
-	//   -->need to ask input one more time and tell customer that the goods hase only X left
-	// 	 X is the quantity of that goods in inventory
-	// return the valid id input
+	//   If we don't have enough goods, tell customer that the goods has only X left
+	//   and return INVALID
+	// 	 
+	// 2.return the valid id input
+	//   input 'b' means back to choose id --> return BACK
 	int inputQuantity();
 
 
