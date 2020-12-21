2bf6ba4e750450b66eafe77e1e00853617c206a8
YJack <jackleagends@gmail.com>
2020-12-21 19:56:03 +0800

    時間轉換

---
+++ HeaderFile/ConvertorOfTimeAndString.h
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <sstream>
+
+string getTimeString(){
+    time_t t=std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::stringstream ss;
+    ss<<std::put_time(std::localtime(&t),"%F %X");
+    ss.str();
+}
+
+time_t StringToDatetime(std::string str)
+{
+    char *cha = (char*)str.data();             // 将string转换成char*。
+    tm tm_;                                    // 定义tm结构体。
+    int year, month, day, hour, minute, second;// 定义时间的各个int临时变量。
+    sscanf(cha, "%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second);// 将string存储的日期时间，转换为int临时变量。
+    tm_.tm_year = year - 1900;                 // 年，由于tm结构体存储的是从1900年开始的时间，所以tm_year为int临时变量减去1900。
+    tm_.tm_mon = month - 1;                    // 月，由于tm结构体的月份存储范围为0-11，所以tm_mon为int临时变量减去1。
+    tm_.tm_mday = day;                         // 日。
+    tm_.tm_hour = hour;                        // 时。
+    tm_.tm_min = minute;                       // 分。
+    tm_.tm_sec = second;                       // 秒。
+    tm_.tm_isdst = 0;                          // 非夏令时。
+    time_t t_ = mktime(&tm_);                  // 将tm结构体转换成time_t格式。
+    return t_;                                 // 返回值。
+}
