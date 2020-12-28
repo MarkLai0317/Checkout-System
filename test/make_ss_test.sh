ba5257620159614d11e01b20e7c9a5c9b99de33a
YJack <jackleagends@gmail.com>
2020-12-28 10:25:23 +0800

    search system 簡易compile

--- test/make_ss_test.sh
+++ test/make_ss_test.sh
@@ -1,1 +1,1 @@
-g++ test_file_search_sys ../SourceFile/FileConnector.cpp ../SourceFile/SearchSystem.cpp -o test_file_search_sys
+g++ test_file_search_sys.cpp ../SourceFile/FileConnector.cpp ../SourceFile/SearchSystem.cpp -o test_file_search_sys
