17213238560952219fa305ae111dadd4b38445db
YJack <jackleagends@gmail.com>
2021-01-08 18:06:44 +0800

    改完test跟有些情況可以直接按按鍵當指令

--- HeaderFile/UserInterface.h
+++ HeaderFile/UserInterface.h
@@ -2,6 +2,8 @@
 #define _USERINTERFACE_H
 
 #include <vector>
+#include <unistd.h>
+#include <termios.h>
 
 #include "GoodInventory.h"
 #include "MainSystem.h"
@@ -147,6 +149,26 @@ class UserInterface{
 
         MainSystem search;
 
+
+		char getKeyboardChar() {
+			char buf = 0;
+			struct termios old = {0};
+			if (tcgetattr(0, &old) < 0)
+					perror("tcsetattr()");
+			old.c_lflag &= ~ICANON;
+			old.c_lflag &= ~ECHO;
+			old.c_cc[VMIN] = 1;
+			old.c_cc[VTIME] = 0;
+			if (tcsetattr(0, TCSANOW, &old) < 0)
+					perror("tcsetattr ICANON");
+			if (read(0, &buf, 1) < 0)
+					perror ("read()");
+			old.c_lflag |= ICANON;
+			old.c_lflag |= ECHO;
+			if (tcsetattr(0, TCSADRAIN, &old) < 0)
+					perror ("tcsetattr ~ICANON");
+			return (buf);
+		}
 
 };
 
--- SourceFile/ManagerUI.cpp
+++ SourceFile/ManagerUI.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include "../HeaderFile/ManagerUI.h"
-#include "../HeaderFile/getKeyboardChar.h"
 
 using namespace std;
 
